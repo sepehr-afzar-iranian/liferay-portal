a32367e087254f3253d018ae9f40786c319d49a1
Nader Jafari <n.jafari@sain.ir>
2022-02-01 16:17:26 +0330

    LPR-165 Add sain-fragments-application artifacts to docker image

--- module-builder.sh
+++ module-builder.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 ../gradlew \
-	 apps:dynamic-data-mapping:dynamic-data-mapping-web:deploy \
+	 apps:document-library:document-library-item-selector-web:deploy \
 	 apps:dynamic-data-mapping:dynamic-data-mapping-form-field-type:deploy \
+	 apps:dynamic-data-mapping:dynamic-data-mapping-web:deploy \
+	 apps:frontend-js:frontend-js-aui-web:deploy \
+	 apps:frontend-js:frontend-js-react-web:deploy \
+	 apps:frontend-js:frontend-js-web:deploy \
+	 apps:frontend-taglib:frontend-taglib-clay:deploy \
+	 apps:item-selector:item-selector-taglib:deploy \
+	 apps:item-selector:item-selector-web:deploy \
 	 apps:journal:journal-item-selector-web:deploy \
 	 apps:journal:journal-service:deploy \
-	 apps:journal:journal-web:deploy \
-	 apps:document-library:document-library-item-selector-web:deploy \
-	 apps:item-selector:item-selector-web:deploy \
-	 apps:item-selector:item-selector-taglib:deploy \
-	 apps:dynamic-data-mapping:dynamic-data-mapping-form-field-type:deploy \
-	 apps:frontend-js:frontend-js-aui-web:deploy \
-	 apps:frontend-js:frontend-js-web:deploy
+	 apps:journal:journal-web:deploy
---
+++ module-downloader.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# how to run: ./module-downloader release-sain.properties REPO_URL REPO_USERNAME REPO_PASSWORD EXTENSION_NEXUS_URL
+
+mkdir SAIN-modules
+
+SAIN_REPO_URL=$2
+SAIN_FTP_USER=$3
+SAIN_FTP_PASS=$4
+SAIN_EXTENSION_NEXUS_URL=$5
+
+IFS=','
+array=($(grep -i "sain.modules.repo" $1 | cut -d '=' -f 2))
+for item in ${array[@]}
+do
+	MODULE_NAME=$(echo $item | cut -d ':' -f 1)
+	MODULE_VERSION=$(echo $item | cut -d ':' -f 2)
+
+	# Download Tar file
+	wget --user $3 --password $4 https://$2/repository/$5/$(echo $MODULE_NAME)/$(echo $MODULE_VERSION)/$(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz || exit 1
+
+	# Download MD5 hash
+	wget --user $3 --password $4 https://$2/repository/$5/$(echo $MODULE_NAME)/$(echo $MODULE_VERSION)/$(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz.md5 || exit 1
+
+	# checksum MD5
+	cat $(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz.md5 | md5sum -c - || exit 1
+
+	echo "checksum $MODULE_NAME:$MODULE_VERSION OK."
+
+	# Uncompress file
+	tar -xzf $(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz --directory=SAIN-modules
+done
