f31901816901a07a173c9f32d2960e8bf11f041b
Mohammad Mehdi Tamehri <tamehrimmt@gmail.com>
2023-05-01 14:48:17 +0330

    LPR-216 fixed bugs

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/ExportFormInstanceMVCResourceCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/ExportFormInstanceMVCResourceCommand.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -13,7 +13,6 @@
  */
 
 package com.liferay.dynamic.data.mapping.form.web.internal.portlet.action;
-
 
 import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
 import com.liferay.dynamic.data.mapping.form.web.internal.configuration.DDMFormWebConfiguration;
@@ -27,20 +26,23 @@ import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalServic
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceService;
 import com.liferay.dynamic.data.mapping.util.comparator.DDMFormInstanceRecordModifiedDateComparator;
 import com.liferay.petra.string.CharPool;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.PortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -50,15 +52,6 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
-
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author Marcellus Tavares
@@ -76,7 +69,7 @@ public class ExportFormInstanceMVCResourceCommand
 
 	@Override
 	protected void doServeResource(
-		ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 		throws Exception {
 
 		String fileExtension = ParamUtil.getString(
@@ -92,24 +85,18 @@ public class ExportFormInstanceMVCResourceCommand
 			return;
 		}
 
-		ThemeDisplay themeDisplay = (ThemeDisplay) resourceRequest.getAttribute(
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		/*Date Date = getDate(resourceRequest,themeDisplay.getTimeZone(),themeDisplay.getLocale(),
-			"DateDay","DateMonth","DateYear",
-			"DateAmPm","DateHour","DateMinute"
-		);
-		System.out.println("Date = " + Date);*/
-		Date startDate = getDate(resourceRequest,
-			"startDateDay","startDateMonth","startDateYear",
-			"startDateAmPm","startDateHour","startDateMinute"
-		);
-		Date endDate = getDate(resourceRequest,
-			"endDateDay","endDateMonth","endDateYear",
-			"endDateAmPm","endDateHour","endDateMinute"
-		);
-		boolean startEnable = ParamUtil.getBoolean(resourceRequest,"startEnable");
-		boolean endEnable = ParamUtil.getBoolean(resourceRequest,"endEnable");
+		Date startDate = _getDate(
+			resourceRequest,themeDisplay, "startDateDay", "startDateMonth", "startDateYear",
+			"startDateAmPm", "startDateHour", "startDateMinute");
+		Date endDate = _getDate(
+			resourceRequest,themeDisplay,"endDateDay", "endDateMonth", "endDateYear",
+			"endDateAmPm", "endDateHour", "endDateMinute");
+		boolean startEnable = ParamUtil.getBoolean(
+			resourceRequest, "startEnable");
+		boolean endEnable = ParamUtil.getBoolean(resourceRequest, "endEnable");
 
 		long formInstanceId = ParamUtil.getLong(
 			resourceRequest, "formInstanceId");
@@ -119,95 +106,81 @@ public class ExportFormInstanceMVCResourceCommand
 				formInstanceId, fileExtension);
 		OrderByComparator<DDMFormInstanceRecord> orderByComparator =
 			_getDDMFormInstanceOrderByComparator("desc");
+
 		List<DDMFormInstanceRecord> ddmFormInstanceRecords =
 			ddmFormInstanceRecordLocalService.getFormInstanceRecords(
 				formInstanceId, 0, -1, -1, orderByComparator);
-		System.out.println(
-			"startDate = " + startDate);
-		System.out.println(
-			"endDate = " + endDate);
-		for (DDMFormInstanceRecord d:ddmFormInstanceRecords
-		) {
-			System.out.println("date: "+d.getModifiedDate());
-		}
 
 		Stream<DDMFormInstanceRecord> recordStream =
 			ddmFormInstanceRecords.stream();
+
 		List<DDMFormInstanceRecord> selectedRecord = recordStream.filter(
 			ddmFormInstanceRecord ->
-				(startEnable ? !startDate.after(ddmFormInstanceRecord.getModifiedDate()):true) &&
-				(endEnable ? !endDate.before(ddmFormInstanceRecord.getModifiedDate()):true)
+				(startEnable ?
+					!startDate.after(ddmFormInstanceRecord.getModifiedDate()) :
+						true) &&
+				(endEnable ?
+					!endDate.before(ddmFormInstanceRecord.getModifiedDate()) :
+						true)
 		).collect(
-			Collectors.toList());
+			Collectors.toList()
+		);
 
-		int start ;
-		int end ;
-		if(selectedRecord.size()==0){
-			start=-2;
+		int start;
+		int end;
+
+		if (selectedRecord.size() == 0) {
+			start = -2;
 			end = -2;
-		}else {
+		}
+		else {
 			start = 0;
 			end = selectedRecord.size();
 		}
 
 		DDMFormInstanceRecordExporterRequest
 			ddmFormInstanceRecordExporterRequest = builder.withLocale(
-			themeDisplay.getLocale()
-		).withStatus(
-			WorkflowConstants.STATUS_APPROVED
-		).withStart(start
-		).withEnd(end
-		).withOrderByComparator(
-			orderByComparator
-		).build();
+				themeDisplay.getLocale()
+			).withStatus(
+				WorkflowConstants.STATUS_APPROVED
+			).withStart(
+				start
+			).withEnd(
+				end
+			).withOrderByComparator(
+				orderByComparator
+			).build();
 
 		DDMFormInstanceRecordExporterResponse
 			ddmFormInstanceRecordExporterResponse =
-			_ddmFormInstanceRecordExporter.export(
-				ddmFormInstanceRecordExporterRequest);
+				_ddmFormInstanceRecordExporter.export(
+					ddmFormInstanceRecordExporterRequest);
 
 		DDMFormInstance formInstance = _ddmFormInstanceService.getFormInstance(
 			formInstanceId);
 
 		String fileName =
 			formInstance.getName(themeDisplay.getLocale()) + CharPool.PERIOD +
-			fileExtension;
-
+				fileExtension;
 
 		PortletResponseUtil.sendFile(
 			resourceRequest, resourceResponse, fileName,
 			ddmFormInstanceRecordExporterResponse.getContent(),
 			MimeTypesUtil.getContentType(fileName));
 	}
-	private Date getDate(ResourceRequest resourceRequest,
-						 String dayParam, String monthParam, String yearParam,
-						 String amPmParam, String hourParam, String minuteParam)
-		throws PortalException {
-		int day = ParamUtil.getInteger(resourceRequest, dayParam);
-		int month = ParamUtil.getInteger(resourceRequest, monthParam);
-		int year = ParamUtil.getInteger(resourceRequest, yearParam);
-		int amPm = ParamUtil.getInteger(resourceRequest, amPmParam);
-		int hour = ParamUtil.getInteger(resourceRequest, hourParam);
-		System.out.println("hour = " + hour);
-		int minute = ParamUtil.getInteger(resourceRequest, minuteParam);
 
-		Calendar calendar = CalendarFactoryUtil.getCalendar();
+	protected void unsetDDMFormWebConfigurationActivator(
+		DDMFormWebConfigurationActivator ddmFormWebConfigurationActivator) {
 
-
-		calendar.set(Calendar.MONTH, month);
-		calendar.set(Calendar.DATE, day);
-		calendar.set(Calendar.YEAR, year);
-		calendar.set(Calendar.HOUR_OF_DAY, hour);
-		calendar.set(Calendar.MINUTE, minute);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
-		calendar.set(Calendar.AM_PM , amPm);
-		System.out.println(calendar.getTime());
-
-		return calendar.getTime();
+		_ddmFormWebConfigurationActivator = null;
 	}
+
+	@Reference
+	protected DDMFormInstanceRecordLocalService
+		ddmFormInstanceRecordLocalService;
+
 	private OrderByComparator<DDMFormInstanceRecord>
-	_getDDMFormInstanceOrderByComparator(String orderByType) {
+		_getDDMFormInstanceOrderByComparator(String orderByType) {
 
 		boolean orderByAsc = false;
 
@@ -218,10 +191,30 @@ public class ExportFormInstanceMVCResourceCommand
 		return new DDMFormInstanceRecordModifiedDateComparator(orderByAsc);
 	}
 
-	protected void unsetDDMFormWebConfigurationActivator(
-		DDMFormWebConfigurationActivator ddmFormWebConfigurationActivator) {
+	private Date _getDate(
+			ResourceRequest resourceRequest,ThemeDisplay themeDisplay, String dayParam, String monthParam,
+			String yearParam, String amPmParam, String hourParam,
+			String minuteParam){
 
-		_ddmFormWebConfigurationActivator = null;
+		int day = ParamUtil.getInteger(resourceRequest, dayParam);
+		int month = ParamUtil.getInteger(resourceRequest, monthParam);
+		int year = ParamUtil.getInteger(resourceRequest, yearParam);
+		int amPm = ParamUtil.getInteger(resourceRequest, amPmParam);
+		int hour = ParamUtil.getInteger(resourceRequest, hourParam);
+		int minute = ParamUtil.getInteger(resourceRequest, minuteParam);
+
+		Calendar calendar = CalendarFactoryUtil.getCalendar(themeDisplay.getTimeZone(),themeDisplay.getLocale());
+
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.DATE, day);
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.HOUR, hour);
+		calendar.set(Calendar.MINUTE, minute);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		calendar.set(Calendar.AM_PM, amPm);
+
+		return calendar.getTime();
 	}
 
 	@Reference
@@ -238,11 +231,5 @@ public class ExportFormInstanceMVCResourceCommand
 	)
 	private volatile DDMFormWebConfigurationActivator
 		_ddmFormWebConfigurationActivator;
-
-	@Reference
-	protected DDMFormInstanceRecordLocalService
-		ddmFormInstanceRecordLocalService;
-	@Reference
-	protected Portal _portal;
 
 }
