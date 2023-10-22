a36ae2466a5f4bfa551857687b8be6461e4e13f4
Mohammad Mehdi Tamehri <tamehrimmt@gmail.com>
2023-10-22 14:54:05 +0330

    LPR-215 Add DDMFormInstanceRecordExporterByTrackingCode to Condition of Export By Price Field.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/ExportFormInstanceMVCResourceCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/ExportFormInstanceMVCResourceCommand.java
@@ -18,12 +18,18 @@ import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
 import com.liferay.dynamic.data.mapping.form.web.internal.configuration.DDMFormWebConfiguration;
 import com.liferay.dynamic.data.mapping.form.web.internal.configuration.activator.DDMFormWebConfigurationActivator;
 import com.liferay.dynamic.data.mapping.io.exporter.DDMFormInstanceRecordExporter;
+import com.liferay.dynamic.data.mapping.io.exporter.DDMFormInstanceRecordExporterByTrackingCode;
 import com.liferay.dynamic.data.mapping.io.exporter.DDMFormInstanceRecordExporterRequest;
 import com.liferay.dynamic.data.mapping.io.exporter.DDMFormInstanceRecordExporterResponse;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceVersionLocalService;
 import com.liferay.dynamic.data.mapping.util.comparator.DDMFormInstanceRecordModifiedDateComparator;
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.portlet.PortletResponseUtil;
@@ -35,6 +41,7 @@ import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
@@ -159,13 +166,29 @@ public class ExportFormInstanceMVCResourceCommand
 				orderByComparator
 			).build();
 
-		DDMFormInstanceRecordExporterResponse
-			ddmFormInstanceRecordExporterResponse =
-				_ddmFormInstanceRecordExporter.export(
-					ddmFormInstanceRecordExporterRequest);
 
 		DDMFormInstance formInstance = _ddmFormInstanceService.getFormInstance(
 			formInstanceId);
+
+		List<DDMFormField> ddmFormFieldList = getFormFields(formInstance);
+		boolean formHasPriceField= false;
+
+		for (DDMFormField ddmFormField : ddmFormFieldList) {
+			if (Validator.isNotNull(ddmFormField.getProperty("priceField")) && (Boolean)ddmFormField.getProperty("priceField")) {
+				formHasPriceField = true;
+				break;
+			}
+		}
+		DDMFormInstanceRecordExporterResponse
+			ddmFormInstanceRecordExporterResponse;
+
+		if (formHasPriceField)
+			ddmFormInstanceRecordExporterResponse=_ddmFormInstanceRecordExporter.export(
+					ddmFormInstanceRecordExporterRequest);
+		else
+			ddmFormInstanceRecordExporterResponse=_ddmFormInstanceRecordExporterByTrackingCode.export(
+				ddmFormInstanceRecordExporterRequest);
+
 
 		String fileName =
 			formInstance.getName(themeDisplay.getLocale()) + CharPool.PERIOD +
@@ -175,6 +198,12 @@ public class ExportFormInstanceMVCResourceCommand
 			resourceRequest, resourceResponse, fileName,
 			ddmFormInstanceRecordExporterResponse.getContent(),
 			MimeTypesUtil.getContentType(fileName));
+	}
+
+	protected List<DDMFormField> getFormFields(DDMFormInstance ddmFormInstance) throws Exception {
+
+		DDMStructure ddmStructure= ddmFormInstance.getStructure();
+		return ddmStructure.getDDMFormFields(false);
 	}
 
 	protected void unsetDDMFormWebConfigurationActivator(
@@ -227,6 +256,10 @@ public class ExportFormInstanceMVCResourceCommand
 
 	@Reference
 	private DDMFormInstanceRecordExporter _ddmFormInstanceRecordExporter;
+
+	@Reference
+	private DDMFormInstanceRecordExporterByTrackingCode
+		_ddmFormInstanceRecordExporterByTrackingCode;
 
 	@Reference
 	private DDMFormInstanceService _ddmFormInstanceService;
