af0f1bc02befc9c0cf75b10d93c115ee2d89f4e3
Yousef Ghadiri <ghadiriyosef123@gmail.com>
2024-07-04 11:09:31 +0330

    DENESHBN-16 Implemented Audit for Group

---
+++ modules/apps/portal-security-audit/portal-security-audit-event-generators-user-management/src/main/java/com/liferay/portal/security/audit/event/generators/user/management/internal/model/listener/GroupModelListener.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.audit.event.generators.user.management.internal.model.listener;
+
+import com.liferay.portal.kernel.audit.AuditMessage;
+import com.liferay.portal.kernel.audit.AuditRouter;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.security.audit.event.generators.constants.EventTypes;
+import com.liferay.portal.security.audit.event.generators.util.Attribute;
+import com.liferay.portal.security.audit.event.generators.util.AttributesBuilder;
+import com.liferay.portal.security.audit.event.generators.util.AuditMessageBuilder;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.List;
+
+/**
+ * @author Yousef Ghadiri
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class GroupModelListener extends BaseModelListener<Group> {
+
+	@Override
+	public void onAfterCreate(Group group)
+		throws ModelListenerException {
+		audit(EventTypes.ADD, group);
+	}
+
+	@Override
+	public void onAfterRemove(Group group)
+		throws ModelListenerException {
+		audit(EventTypes.DELETE, group);
+	}
+
+	@Override
+	public void onBeforeUpdate(Group newGroup)
+		throws ModelListenerException {
+
+		try {
+			long groupId = newGroup.getGroupId();
+			Group oldGroup = _groupLocalService.getGroup(groupId);
+
+			List<Attribute> attributes = getModifiedAttributes(
+				newGroup, oldGroup);
+
+			AuditMessage auditMessage = AuditMessageBuilder.buildAuditMessage(
+				EventTypes.UPDATE, Group.class.getName(), groupId, attributes);
+
+			JSONObject additionalInfoJSONObject =
+				auditMessage.getAdditionalInfo();
+
+			additionalInfoJSONObject.put(
+				"userName", newGroup.getName()
+			).put(
+				"groupId", groupId
+			);
+
+			_auditRouter.route(auditMessage);
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
+	protected List<Attribute> getModifiedAttributes(
+		Group newGroup, Group oldGroup) {
+
+		AttributesBuilder attributesBuilder = new AttributesBuilder(
+			newGroup, oldGroup);
+
+		attributesBuilder.add("name");
+
+		return attributesBuilder.getAttributes();
+	}
+
+	protected void audit(String eventType, Group group)
+		throws ModelListenerException {
+
+		try {
+			long groupId = group.getGroupId();
+			AuditMessage auditMessage = AuditMessageBuilder.buildAuditMessage(
+				eventType, Group.class.getName(), groupId, null);
+
+			JSONObject additionalInfoJSONObject =
+				auditMessage.getAdditionalInfo();
+
+			additionalInfoJSONObject.put(
+				"userName", group.getName()
+			).put(
+				"groupId", groupId
+			);
+
+			_auditRouter.route(auditMessage);
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
+	@Reference
+	private AuditRouter _auditRouter;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+}
