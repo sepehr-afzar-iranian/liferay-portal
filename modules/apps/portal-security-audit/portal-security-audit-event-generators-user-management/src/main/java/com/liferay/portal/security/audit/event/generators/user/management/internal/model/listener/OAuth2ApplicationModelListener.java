9d87a43503a70246e5065c6d23f6ff63dbcb0555
Yousef Ghadiri <ghadiriyosef123@gmail.com>
2024-07-04 11:03:13 +0330

    DENESHBN-16 Implemented Audit for OAuth2 Application

---
+++ modules/apps/portal-security-audit/portal-security-audit-event-generators-user-management/src/main/java/com/liferay/portal/security/audit/event/generators/user/management/internal/model/listener/OAuth2ApplicationModelListener.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.audit.event.generators.user.management.internal.model.listener;
+
+import com.liferay.oauth2.provider.model.OAuth2Application;
+import com.liferay.portal.kernel.audit.AuditMessage;
+import com.liferay.portal.kernel.audit.AuditRouter;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.security.audit.event.generators.constants.EventTypes;
+import com.liferay.portal.security.audit.event.generators.util.AuditMessageBuilder;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Yousef Ghadiri
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class OAuth2ApplicationModelListener extends BaseModelListener<OAuth2Application> {
+
+	@Override
+	public void onAfterCreate(OAuth2Application oAuth2Application)
+		throws ModelListenerException {
+		audit(EventTypes.ADD, oAuth2Application);
+	}
+
+	@Override
+	public void onAfterRemove(OAuth2Application oAuth2Application)
+		throws ModelListenerException {
+		audit(EventTypes.DELETE, oAuth2Application);
+	}
+
+	@Override
+	public void onAfterUpdate(OAuth2Application oAuth2Application)
+		throws ModelListenerException {
+		audit(EventTypes.UPDATE, oAuth2Application);
+	}
+
+	protected void audit(
+		String eventType, OAuth2Application oAuth2Application)
+		throws ModelListenerException {
+
+		try {
+			long oAuth2ApplicationId = oAuth2Application.getOAuth2ApplicationId();
+			AuditMessage auditMessage =
+				AuditMessageBuilder.buildAuditMessage(eventType,
+					OAuth2Application.class.getName(), oAuth2ApplicationId,
+					null);
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			JSONObject additionalInfoJSONObject =
+				auditMessage.getAdditionalInfo();
+
+			additionalInfoJSONObject.put(
+				"groupId", serviceContext.getScopeGroupId()
+			).put(
+				"oAuth2ApplicationId", oAuth2ApplicationId
+			).put(
+				"oAuth2ApplicationName", oAuth2Application.getName()
+			).put(
+				"oAuth2ApplicationRedirectURIs", oAuth2Application.getRedirectURIs()
+			);
+
+			_auditRouter.route(auditMessage);
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
+	@Reference
+	private AuditRouter _auditRouter;
+
+}
