cf7031964979eb151fffdba2451953d3fb874b20
Yousef Ghadiri <ghadiriyosef123@gmail.com>
2024-07-04 11:06:23 +0330

    DENESHBN-16 Implemented Audit for Blogs

---
+++ modules/apps/portal-security-audit/portal-security-audit-event-generators-user-management/src/main/java/com/liferay/portal/security/audit/event/generators/user/management/internal/model/listener/BlogsEntryModelListener.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.audit.event.generators.user.management.internal.model.listener;
+
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.portal.kernel.audit.AuditMessage;
+import com.liferay.portal.kernel.audit.AuditRouter;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.security.audit.event.generators.constants.EventTypes;
+import com.liferay.portal.security.audit.event.generators.util.AuditMessageBuilder;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Yousef Ghadiri
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class BlogsEntryModelListener extends BaseModelListener<BlogsEntry> {
+
+	@Override
+	public void onAfterCreate(BlogsEntry blogsEntry)
+		throws ModelListenerException {
+		audit(EventTypes.ADD, blogsEntry);
+	}
+
+	@Override
+	public void onAfterUpdate(BlogsEntry blogsEntry)
+		throws ModelListenerException {
+		audit(EventTypes.UPDATE, blogsEntry);
+	}
+
+	@Override
+	public void onAfterRemove(BlogsEntry blogsEntry)
+		throws ModelListenerException {
+		audit(EventTypes.DELETE, blogsEntry);
+	}
+
+	protected void audit(
+		String eventType, BlogsEntry blogsEntry)
+		throws ModelListenerException {
+
+		try {
+			long blogsEntryId = blogsEntry.getEntryId();
+			AuditMessage auditMessage =
+				AuditMessageBuilder.buildAuditMessage(eventType,
+					BlogsEntry.class.getName(), blogsEntryId,
+					null);
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			JSONObject additionalInfoJSONObject =
+				auditMessage.getAdditionalInfo();
+
+			additionalInfoJSONObject.put(
+				"groupId", serviceContext.getScopeGroupId()
+			).put(
+				"blogsEntryId", blogsEntryId
+			).put(
+				"blogsEntryTitle", blogsEntry.getTitle()
+			);
+
+			_auditRouter.route(auditMessage);
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
+	@Reference
+	private AuditRouter _auditRouter;
+
+}
