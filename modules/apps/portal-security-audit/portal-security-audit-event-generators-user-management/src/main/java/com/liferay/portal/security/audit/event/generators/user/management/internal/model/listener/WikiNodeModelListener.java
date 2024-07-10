9e2d85e72ec7bd6dace41320f13c1070e4224655
Yousef Ghadiri <ghadiriyosef123@gmail.com>
2024-07-10 10:11:02 +0330

    DENESHBN-16 Implemented Audit for Wiki

---
+++ modules/apps/portal-security-audit/portal-security-audit-event-generators-user-management/src/main/java/com/liferay/portal/security/audit/event/generators/user/management/internal/model/listener/WikiNodeModelListener.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.audit.event.generators.user.management.internal.model.listener;
+
+import com.liferay.portal.kernel.audit.AuditMessage;
+import com.liferay.portal.kernel.audit.AuditRouter;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.security.audit.event.generators.constants.EventTypes;
+import com.liferay.portal.security.audit.event.generators.util.AuditMessageBuilder;
+import com.liferay.wiki.model.WikiNode;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Yousef Ghadiri
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class WikiNodeModelListener extends BaseModelListener<WikiNode> {
+
+	@Override
+	public void onAfterCreate(WikiNode wikiNode) throws ModelListenerException {
+		audit(EventTypes.ADD, wikiNode);
+	}
+
+	@Override
+	public void onAfterRemove(WikiNode wikiNode) throws ModelListenerException {
+		audit(EventTypes.DELETE, wikiNode);
+	}
+
+	@Override
+	public void onAfterUpdate(WikiNode wikiNode) throws ModelListenerException {
+		audit(EventTypes.UPDATE, wikiNode);
+	}
+
+	protected void audit(
+		String eventType, WikiNode wikiNode)
+		throws ModelListenerException {
+		try {
+			long wikiNodeId = wikiNode.getNodeId();
+			AuditMessage auditMessage =
+				AuditMessageBuilder.buildAuditMessage(eventType,
+					WikiNode.class.getName(), wikiNodeId,
+					null);
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			JSONObject additionalInfoJSONObject =
+				auditMessage.getAdditionalInfo();
+
+			additionalInfoJSONObject.put(
+				"groupId", serviceContext.getScopeGroupId()
+			).put(
+				"wikiNodeId", wikiNodeId
+			).put(
+				"wikiNodeName", wikiNode.getName()
+			);
+
+			_auditRouter.route(auditMessage);
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
+	@Reference
+	private AuditRouter _auditRouter;
+
+}
---
+++ modules/apps/portal-security-audit/portal-security-audit-event-generators-user-management/src/main/java/com/liferay/portal/security/audit/event/generators/user/management/internal/model/listener/WikiPageModelListener.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.audit.event.generators.user.management.internal.model.listener;
+
+import com.liferay.portal.kernel.audit.AuditMessage;
+import com.liferay.portal.kernel.audit.AuditRouter;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.security.audit.event.generators.constants.EventTypes;
+import com.liferay.portal.security.audit.event.generators.util.AuditMessageBuilder;
+import com.liferay.wiki.model.WikiPage;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Yousef Ghadiri
+ */
+
+@Component(immediate = true, service = ModelListener.class)
+public class WikiPageModelListener extends BaseModelListener<WikiPage> {
+
+	@Override
+	public void onAfterCreate(WikiPage wikiPage) throws ModelListenerException {
+		audit(EventTypes.ADD, wikiPage);
+	}
+
+	@Override
+	public void onAfterRemove(WikiPage wikiPage) throws ModelListenerException {
+		audit(EventTypes.DELETE, wikiPage);
+	}
+
+	@Override
+	public void onAfterUpdate(WikiPage wikiPage) throws ModelListenerException {
+		audit(EventTypes.UPDATE, wikiPage);
+	}
+
+	protected void audit(
+		String eventType, WikiPage wikiPage)
+		throws ModelListenerException {
+		try {
+			long wikiPageId = wikiPage.getPageId();
+			AuditMessage auditMessage =
+				AuditMessageBuilder.buildAuditMessage(eventType,
+					WikiPage.class.getName(), wikiPageId,
+					null);
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			JSONObject additionalInfoJSONObject =
+				auditMessage.getAdditionalInfo();
+
+			additionalInfoJSONObject.put(
+				"groupId", serviceContext.getScopeGroupId()
+			).put(
+				"wikiPageId", wikiPageId
+			).put(
+				"wikiPageTitle", wikiPage.getTitle()
+			).put(
+				"wikiNodeId", wikiPage.getNodeId()
+			).put(
+				"wikiNodeName", wikiPage.getNode().getName()
+			);
+
+			_auditRouter.route(auditMessage);
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
+	@Reference
+	private AuditRouter _auditRouter;
+
+}
