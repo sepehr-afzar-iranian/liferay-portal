86b9f16ee41110102f96c85d4b2cb1709d181f5a
Nader Jafari <n.jafari@sain.ir>
2024-07-27 11:03:37 +0330

    Add Dockerfile.pure to build all portal modules

--- scripts/extension_mover.sh
+++ scripts/extension_mover.sh
@@ -3,6 +3,6 @@
 cp /opt/liferay/extensions/*.jar /opt/liferay/osgi/modules
 cp /opt/liferay/extensions/*.war /opt/liferay/osgi/war
 rm -Rf /opt/liferay/osgi/marketplace/Liferay\ CE\ Static\ -\ *
-rm -Rf /opt/liferay/osgi/state/*
+rm -Rf /opt/liferay/osgi/state
 rm -Rf /opt/liferay/tomcat-9.0.43/work/*
 rm -Rf /opt/liferay/tomcat-9.0.43/temp/*
