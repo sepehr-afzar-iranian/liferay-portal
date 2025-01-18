e27874820effb2796fdd0f1e8840a6d1067b5d23
Yousef Ghadiri <ghadiriyosef123@gmail.com>
2025-01-18 15:52:59 +0330

    DNSHBN-23 Moved Invalidating User's Sessions to AuthenticatedSessionManagerImpl

--- modules/apps/portal-security-audit/portal-security-audit-event-generators-user-management/src/main/java/com/liferay/portal/security/audit/event/generators/user/management/internal/events/LoginPostAction.java
+++ modules/apps/portal-security-audit/portal-security-audit-event-generators-user-management/src/main/java/com/liferay/portal/security/audit/event/generators/user/management/internal/events/LoginPostAction.java
@@ -22,18 +22,12 @@ import com.liferay.portal.kernel.events.LifecycleAction;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.servlet.PortalSessionContext;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.security.audit.event.generators.constants.EventTypes;
 import com.liferay.portal.security.audit.event.generators.user.management.util.AuditMessageHelperUtil;
 
-import java.util.Collection;
-import java.util.Objects;
-import java.util.stream.Stream;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -67,28 +61,6 @@ public class LoginPostAction extends Action {
 					EventTypes.LOGIN, null, userFullName, 0));
 
 			_auditRouter.route(auditMessage);
-
-			Collection<HttpSession> sessions = PortalSessionContext.values();
-
-			Stream<HttpSession> stream = sessions.stream();
-
-			stream.filter(
-				session ->
-					Objects.equals(userId, session.getAttribute("USER_ID")) &&
-					!Objects.equals(session, httpServletRequest.getSession())
-			).forEach(
-				session -> {
-					try {
-						session.invalidate();
-					}
-					catch (Exception exception) {
-						if (_log.isWarnEnabled()) {
-							_log.warn(
-								"Unable to invalidate session", exception);
-						}
-					}
-				}
-			);
 		}
 		catch (Exception exception) {
 			if (_log.isWarnEnabled()) {
