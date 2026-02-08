bc43335a1901af1d5828401a3243597ceaa48a12
Yousef Ghadiri <ghadiriyosef123@gmail.com>
2026-02-08 14:34:42 +0330

    DNSHBN-80 Make module-downloader.sh offline

--- module-downloader.sh
+++ module-downloader.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
-# how to run: ./module-downloader release-sain.properties REPO_URL REPO_USERNAME REPO_PASSWORD EXTENSION_NEXUS_URL
+# how to run: ./module-downloader release-sain.properties
+# Note: Files should be committed in ./downloaded-modules/ directory
 
-mkdir SAIN-modules
+mkdir -p SAIN-modules
 
-SAIN_REPO_URL=$2
-SAIN_FTP_USER=$3
-SAIN_FTP_PASS=$4
-SAIN_EXTENSION_NEXUS_URL=$5
+# Path to where the downloaded files are stored in the repo
+DOWNLOADED_MODULES_DIR="./downloaded-modules"
 
 IFS=','
 array=($(grep -i "sain.modules.repo" $1 | cut -d '=' -f 2))
 for item in ${array[@]}
 do
-	MODULE_NAME=$(echo $item | cut -d ':' -f 1)
-	MODULE_VERSION=$(echo $item | cut -d ':' -f 2)
+  MODULE_NAME=$(echo $item | cut -d ':' -f 1)
+  MODULE_VERSION=$(echo $item | cut -d ':' -f 2)
 
-	# Download Tar file
-	wget --user $3 --password $4 https://$2/repository/$5/$(echo $MODULE_NAME)/$(echo $MODULE_VERSION)/$(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz || exit 1
+  TAR_FILE="$(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz"
 
-	# Download MD5 hash
-	wget --user $3 --password $4 https://$2/repository/$5/$(echo $MODULE_NAME)/$(echo $MODULE_VERSION)/$(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz.md5 || exit 1
+  # Check if tar file exists
+  if [ ! -f "$DOWNLOADED_MODULES_DIR/$TAR_FILE" ]; then
+    echo "Error: $TAR_FILE not found in $DOWNLOADED_MODULES_DIR"
+    exit 1
+  fi
 
-	# checksum MD5
-	cat $(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz.md5 | md5sum -c - || exit 1
-
-	echo "checksum $MODULE_NAME:$MODULE_VERSION OK."
-
-	# Uncompress file
-	tar -xzf $(echo $MODULE_NAME)-$(echo $MODULE_VERSION).tar.gz --directory=SAIN-modules
+  # Uncompress file
+  tar -xzf "$DOWNLOADED_MODULES_DIR/$TAR_FILE" --directory=SAIN-modules
 done
 
-# Download static contents
+# Extract static contents
+if [ ! -f "$DOWNLOADED_MODULES_DIR/fonts.tgz" ]; then
+  echo "Error: fonts.tgz not found in $DOWNLOADED_MODULES_DIR"
+  exit 1
+fi
 
-wget --user $3 --password $4 https://$2/repository/$5/static/fonts.tgz || exit 1
-tar -xzf ./fonts.tgz --directory=./
+tar -xzf "$DOWNLOADED_MODULES_DIR/fonts.tgz" --directory=./
